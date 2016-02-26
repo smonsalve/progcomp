6a1494fa0983ab6ce0067f43054813add651d0ae
Sergio Monsalve <smonsalve@gmail.com>
2016-02-26 15:20:36 -0500

    Cleaning Up

---
+++ 6.Aux/lectura.py
@@ -0,0 +1,20 @@
+Semana4 = []
+
+Capitulo0 = 0
+Capitulo1 = 0
+Capitulo2 = 1
+Capitulo3 = 1
+Capitulo4 = 4
+Capitulo5 = 10
+
+
+Semana5 = []
+
+Capitulo0 = 0
+Capitulo1 = 0
+Capitulo2 = 1
+Capitulo3 = 2
+Capitulo4 = 2
+Capitulo5 = 10
+Capitulo6 = 3
+
--- lectura.py
+++
@@ -1,8 +0,0 @@
-Semana = 4
-
-Capitulo1 = 
-Capitulo2 = 1
-Capitulo3 = 1
-Capitulo4 = 4
-Capitulo5 = 10
-
