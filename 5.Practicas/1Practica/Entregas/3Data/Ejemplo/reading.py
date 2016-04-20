595385f3bf4a28636da4f125048baa81efe5a0f7
Sergio Monsalve <smonsalve@gmail.com>
2016-04-20 14:24:43 -0500

    globurl=noglob urlglobber

--- 5.Practicas/1Practica/Entregas/3Data/Ejemplo/reading.py
+++ 5.Practicas/1Practica/Entregas/3Data/Ejemplo/reading.py
@@ -1,24 +1,28 @@
 # for linea in open("Hola.txt"):
 #     print linea
 
-nomArchivo = "Hola.txt"
+# nomArchivo = "Hola.txt"
 
 # archivo = open(nomArchivo)
 # algo = len(archivo.readlines())
 # print algo
 
-def esVacio(archivo):
-    contenido = True
-    elfile = open(archivo)
-    lineas = len(elfile.readlines())
-    #print lineas
-    if lineas != 0:
-        elfile = open(archivo)
-        for line in elfile:
-            print line
-            if line != "\n":
-                contenido = False
-    return contenido
+# def esVacio(archivo):
+#     contenido = True
+#     elfile = open(archivo)
+#     lineas = len(elfile.readlines())
+#     #print lineas
+#     if lineas != 0:
+#         elfile = open(archivo)
+#         for line in elfile:
+#             print line
+#             if line != "\n":
+#                 contenido = False
+#     return contenido
 
 
-print esVacio(nomArchivo)
+# print esVacio(nomArchivo)
+
+
+import math
+print math.sqrt(-1)
