b4b7b6a672a94ebc9568f088776eda53fde30037
Sergio Monsalve <smonsalve@gmail.com>
2016-05-06 11:54:38 -0500

    Mayo 6

---
+++ 2.Ejercicios/Dig.m
@@ -0,0 +1,24 @@
+a = [1,0,0;0,1,0;0,0,1];
+%disp(a)
+
+function es_diagonal(orden, mat)
+	diag = 1;
+	for i=1:orden
+		for j=1:orden
+			if (i!=j)
+				%printf('i:%d j:%d',i,j)
+				if (mat(i,j)!=0)
+					diag = 0;
+				end
+			end
+		end
+	end
+	if (diag == 1)
+		printf('Verdadero\n')
+	else
+		printf('Falso\n')
+	end
+end
+
+es_diagonal(3,a)
+
